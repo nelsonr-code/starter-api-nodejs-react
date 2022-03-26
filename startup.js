39153c47e9472d9b3943767dd0f5975a1aed88ff
Nelson Rivas <nelson.rivas.ucv@gmail.com>
2022-03-26 19:54:52 -0400

    startup server

--- startup.js
+++ startup.js
@@ -1,7 +1,18 @@
-import { app } from "./startup";
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import server from "~/config/server";
 
-server.start();
+
+const startup = async () => {
+    try {
+        await server.start();
+        
+    } catch (error) {
+        console.error(`An error ocurred: ${error}`);
+    }
+}
+
+startup();
 
 // middlewares
 // app.use(morgan('dev'));
