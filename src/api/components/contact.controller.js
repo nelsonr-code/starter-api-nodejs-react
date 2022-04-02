fa17a26d7c54508b4dc3dfbf40b581fcf3d4ade6
Nelson Rivas <nelson.rivas.ucv@gmail.com>
2022-04-02 11:36:30 -0400

    crud completed

--- src/api/components/contact.controller.js
+++ src/api/components/contact.controller.js
@@ -1,7 +1,6 @@
 import DBFirestore from "~/services/firebase/connection";
 
-export const getContacts = (async (req, res) => {
-    
+export const getAllContacts = (async (req, resp) => {
     try {
         const querySnapshot = await DBFirestore.collection('contacts').get();
         const allContacts = querySnapshot.docs.map(doc => ({
@@ -13,10 +12,81 @@ export const getContacts = (async (req, res) => {
             ...doc.data()
         }))
         console.log(allContacts);
-        res.json({ "all contacts": allContacts });
-            
+        resp.json({ "all contacts": allContacts });
+
     } catch (error) {
-        console.error(`An error ocurred getting contacts: ${error}`);        
+        console.error(`An error ocurred getting contacts: ${error}`);
     }
 
 })
+
+export const getContact = async (req, resp) => {
+    try {
+        const idContact = req.params.id;
+
+        const doc = await DBFirestore
+            .collection('contacts')
+            .doc(idContact)
+            .get()
+
+        resp.send(doc.data());
+
+    } catch (error) {
+
+    }
+}
+
+export const addContact = async (req, resp) => {
+    try {
+        const {
+            firstname,
+            lastname,
+            phone,
+            email
+        } = req.body;
+
+        await DBFirestore.collection('contacts').add({
+            firstname,
+            lastname,
+            phone,
+            email
+        })
+
+        resp.send("Contact added");
+
+    } catch (error) {
+
+    }
+}
+
+export const editContact = async (req, resp) => {
+    try {
+        const idContact = req.params.id;
+
+        await DBFirestore
+            .collection('contacts')
+            .doc(idContact)
+            .update(req.body)
+
+        resp.send("contact edited");
+
+    } catch (error) {
+
+    }
+}
+
+export const deleteContact = async (req, res) => {
+    try {
+        const idContact = req.params.id;
+
+        await DBFirestore
+            .collection('contacts')
+            .doc(idContact)
+            .delete()
+
+        res.send("contact deleted");
+
+    } catch (error) {
+
+    }
+}
--- src/api/components/contact.routes.js
+++ src/api/components/contact.routes.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { getContacts } from "./contact.controller";
+import {
+    getAllContacts, getContact,
+    addContact, editContact,
+    deleteContact
+} from "./contact.controller";
 
 const ContactRouter = Router();
 
-ContactRouter.get("/", getContacts);
+ContactRouter.get("/", getAllContacts);
+ContactRouter.get("/show-contact", getContact);
+ContactRouter.post("/add-contact", addContact);
+ContactRouter.post("/edit-contact/:id", editContact);
+ContactRouter.get("/delete-contact/:id", deleteContact);
 
 export default ContactRouter;
